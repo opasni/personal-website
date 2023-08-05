43bb01bd915665c16495c700c6772ca54737664a
Crt Harej <crt.harej@achtzig20.de>
2023-08-05 16:36:47 +0200

    add(cv): added the cv module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,33 +3,38 @@ import { RouterModule, Routes } from '@angular/router';
 import { BackboneComponent } from './components/backbone/backbone.component';
 import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [{
-    path: '',
-    component: BackboneComponent,
-    children: [
-        {
-            path: '',
-            redirectTo: 'about',
-            pathMatch: 'full'
-        }, {
-            path: 'about',
-            component: AboutComponent,
-            data: { animation: 0 }
-        }, {
-            path: 'skills',
-            loadChildren: () => import('./modules/skills/skills.module').then((m) => m.SkillsModule),
-        }, {
-            path: 'timeline',
-            loadChildren: () => import('./modules/timeline/timeline.module').then((m) => m.TimelineModule),
-        }, {
-            path: 'contact',
-            loadChildren: () => import('./modules/contact/contact.module').then((m) => m.ContactModule),
-        }
-    ]
-}];
+const routes: Routes = [
+	{
+		path: '',
+		component: BackboneComponent,
+		children: [
+			{
+				path: '',
+				redirectTo: 'about',
+				pathMatch: 'full'
+			}, {
+				path: 'about',
+				component: AboutComponent,
+				data: { animation: 0 }
+			}, {
+				path: 'skills',
+				loadChildren: () => import('./modules/skills/skills.module').then((m) => m.SkillsModule),
+			}, {
+				path: 'timeline',
+				loadChildren: () => import('./modules/timeline/timeline.module').then((m) => m.TimelineModule),
+			}, {
+				path: 'contact',
+				loadChildren: () => import('./modules/contact/contact.module').then((m) => m.ContactModule),
+			}
+		]
+	}, {
+		path: 'cv',
+		loadChildren: () => import('./modules/cv/cv.module').then((m) => m.CvModule),
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
