df1e844e1de6f046d8c28ce3b0ba051eb407246f
Crt Harej <crt.harej@achtzig20.de>
2023-10-31 13:37:00 +0100

    fix(language): fixed language switch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,19 @@
-import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LanguageService } from '@lib/services/language.service';
+import { LOCALE_PROVIDER, LanguageService } from '@lib/services/language.service';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
 	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
-export const setupLanguageFactory = async (service: LanguageService) => await service.initialize();
+export function initConfig(configService: LanguageService) {
+  return () => configService.initialize();
+}
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -20,6 +22,8 @@ export const appConfig: ApplicationConfig = {
 				withComponentInputBinding(),
 				withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' }),
 		),
+		{ provide: APP_INITIALIZER, deps: [LanguageService], useFactory: initConfig, multi: true },
+		LOCALE_PROVIDER,
 		importProvidersFrom(
 			BrowserAnimationsModule,
 			NgbModule,
@@ -32,6 +36,5 @@ export const appConfig: ApplicationConfig = {
 					}
 			}),
 		),
-		{ provide: LOCALE_ID, deps: [LanguageService], useFactory: setupLanguageFactory },
 	]
 };
