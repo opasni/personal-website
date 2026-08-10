7b67429c26edb4ca91846b8c9df7728dd98d8efc
Crt Harej <crt.harej@achtzig20.de>
2026-08-10 19:36:41 +0200

    add(skills): certifications and small adjustments

--- src/app/lib/components/skills-overview/skills-overview.component.ts
+++ src/app/lib/components/skills-overview/skills-overview.component.ts
@@ -1,21 +1,11 @@
-import { Component, OnInit, ViewEncapsulation, inject } from '@angular/core';
-import { SkillsService } from '@lib/services/skills.service';
-import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { SkillMeasureComponent } from 'src/app/modules/skills/components/skill-measure/skill-measure.component';
 
 @Component({
     selector: 'app-skills-overview',
     templateUrl: './skills-overview.component.html',
     styleUrls: ['./skills-overview.component.scss'],
-    providers: [SkillsService],
     encapsulation: ViewEncapsulation.None,
-    imports: [CommonModule, SkillMeasureComponent, TranslateModule],
+    imports: [TranslateModule],
 })
-export class SkillsOverviewComponent implements OnInit {
-    private _skills = inject(SkillsService);
-
-    ngOnInit(): void {
-        this._skills.minified = true;
-    }
-}
+export class SkillsOverviewComponent {}
